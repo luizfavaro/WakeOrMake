0bd5fad312ea00f40e33c9a555bb6d1d1f969cc6
luiz <luiz@luiz-PC>
2013-12-12 21:00:04 -0200

    Primeira parte da criação do banco de dados do app.

--- WakeOrMake/src/com/android/wakeormake/WakeOrMake.java
+++ WakeOrMake/src/com/android/wakeormake/WakeOrMake.java
@@ -3,9 +3,15 @@ package com.android.wakeormake;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.database.*;
+import android.database.sqlite.*;
 
 public class WakeOrMake extends Activity {
 
+	String NomeBanco = "WakeorMake";   
+	SQLiteDatabase Alarmes = null;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,4 +25,23 @@ public class WakeOrMake extends Activity {
 		return true;
 	}
 
+	public void CriaBanco(){
+		try {
+			Alarmes = openOrCreateDatabase(NomeBanco, MODE_WORLD_READABLE, null);
+			String SQL = "CREATE TABLE IF NOT EXIST tabAlarmes (_id INTEGER PRIMARY KEY, ativo TEXT, hora TEXT, segunda TEXT, terca TEXT, quarta TEXT, quinta TEXT, sexta TEXT, sabado TEXT, domingo TEXT, toque TEXT, titulo TEXT, desbloqueio INTEGER)";
+			Alarmes.execSQL(SQL);    
+		} catch (Exception erro) {
+		}
+	}
+	                        //    S OU N       HH:MM           S OU N        S OU N         S OU N         S OU N        S OU N        S OU N           S OU N  DIRETORIOMUSIC  DIGITAR TITLE   CODIGO TIPO DESBLOQUEIO -> FAZER CLASSE VERIFICAR ESTE CAMPO E EXECUTAR A VALIDAÇÃO.
+	public void GravaBanco( String ativo, String hora, String segunda, String terca, String quarta, String quinta, String sexta, String sabado, String domingo, String toque, String titulo, Integer desbloqueio  ){
+		try {
+			Alarmes = openOrCreateDatabase(NomeBanco, MODE_WORLD_READABLE, null);
+			String SQL = "INSERT INTO tabnAlarmes (ativo, hora, segunda, terca, quarta, quinta, sexta, sabado, domingo, toque, titulo, desbloqueio) VALUES ( ativo = '" + ativo + "', hora = '" + hora + "', segunda = '" + segunda + "', terca = '" + terca + "', quarta = '" + quarta + "', quinta = '" + quinta + "', sexta = '" + sexta +"', sabado = '" + sabado + "', domigo = '" + domingo + "', toque = '" + toque + "', titutlo = '" + titulo + "', desbolqueio = " + Integer.toString(desbloqueio) + ")";
+			Alarmes.execSQL(SQL);
+		} catch (Exception erro) {
+			// TODO: handle exception
+		}
+	}
+		
 }
