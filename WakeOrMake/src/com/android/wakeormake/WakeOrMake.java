ce4e5dff2c6f44126ccd49d0a5e9d3d654e7e791
luizfavaro <luizafonso@sgsistemas.com.br>
2013-12-12 23:48:56 -0200

    Finalizei as funções referentes a Banco de dados.

--- WakeOrMake/src/com/android/wakeormake/WakeOrMake.java
+++ WakeOrMake/src/com/android/wakeormake/WakeOrMake.java
@@ -1,7 +1,12 @@
 package com.android.wakeormake;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.R.array;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
 import android.database.*;
 import android.database.sqlite.*;
@@ -9,8 +14,9 @@ import android.database.sqlite.*;
 public class WakeOrMake extends Activity {
 
 	String NomeBanco = "WakeorMake";   
-	SQLiteDatabase Alarmes = null;
-	
+	static SQLiteDatabase Alarmes = null;
+	static Cursor cursor; 
+	public array AlarmesDaTabela;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,8 @@ public class WakeOrMake extends Activity {
 			String SQL = "CREATE TABLE IF NOT EXIST tabAlarmes (_id INTEGER PRIMARY KEY, ativo TEXT, hora TEXT, segunda TEXT, terca TEXT, quarta TEXT, quinta TEXT, sexta TEXT, sabado TEXT, domingo TEXT, toque TEXT, titulo TEXT, desbloqueio INTEGER)";
 			Alarmes.execSQL(SQL);    
 		} catch (Exception erro) {
-		}
+			MsgInfo( "Por algum motivo desconhecido não consegui criar as tabelas nescessárias! " + erro, "Erro no Banco de Dados!");
+			}
 	}
 	                        //    S OU N       HH:MM           S OU N        S OU N         S OU N         S OU N        S OU N        S OU N           S OU N  DIRETORIOMUSIC  DIGITAR TITLE   CODIGO TIPO DESBLOQUEIO -> FAZER CLASSE VERIFICAR ESTE CAMPO E EXECUTAR A VALIDAÇÃO.
 	public void GravaBanco( String ativo, String hora, String segunda, String terca, String quarta, String quinta, String sexta, String sabado, String domingo, String toque, String titulo, Integer desbloqueio  ){
@@ -40,8 +47,27 @@ public class WakeOrMake extends Activity {
 			String SQL = "INSERT INTO tabnAlarmes (ativo, hora, segunda, terca, quarta, quinta, sexta, sabado, domingo, toque, titulo, desbloqueio) VALUES ( ativo = '" + ativo + "', hora = '" + hora + "', segunda = '" + segunda + "', terca = '" + terca + "', quarta = '" + quarta + "', quinta = '" + quinta + "', sexta = '" + sexta +"', sabado = '" + sabado + "', domigo = '" + domingo + "', toque = '" + toque + "', titutlo = '" + titulo + "', desbolqueio = " + Integer.toString(desbloqueio) + ")";
 			Alarmes.execSQL(SQL);
 		} catch (Exception erro) {
-			// TODO: handle exception
-		}
+			MsgInfo( "Por algum motivo desconhecido não consegui criar as tabelas nescessárias! " + erro, "Erro no Banco de Dados!");
+			}
+	}
+	public ArrayList<Integer> PegaAlarmes(){
+		ArrayList<Integer> AlarmesDaTabela = new ArrayList<Integer>();
+		cursor = Alarmes.rawQuery("SELECT * FROM tabAlarmes", null);
+		cursor.moveToFirst();
+		
+		while( !cursor.isAfterLast() ){
+		    AlarmesDaTabela.add( Integer.parseInt( cursor.getString(0) ) );
+		    cursor.moveToNext();
+		    }
+		return AlarmesDaTabela;
+	}
+	
+	public void MsgInfo( String Mensagem, String Titulo ){
+		AlertDialog.Builder Dialog = new AlertDialog.Builder(WakeOrMake.this);
+		Dialog.setTitle(Titulo);
+		Dialog.setMessage(Mensagem);	
+		Dialog.setNeutralButton("Ok", null);
+		Dialog.show();
 	}
 		
 }
